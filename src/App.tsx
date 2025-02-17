18bc59cebd314dd5f8a08cf9f9cc3886d714e50e
Nishanth <nishanth1596@gmail.com>
2025-02-17 19:20:52 +0530

    fixed responsiveness bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Main from "./Components/Main";
 
 function App() {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col items-center">
       <div className="mx-4 flex flex-col items-center md:mx-8 xl:mx-[8.44rem] xl:max-w-[73.125rem]">
         <Header />
         <Main />
